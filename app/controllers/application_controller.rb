5a13aaada01bbf2e380be5db0678eb4066edd159
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-11-01 13:39:44 +1300

    controller and view fixes for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |user|
-      user.permit(:username, :email, :password, :password_confirmation)
-    end
-  end
+  rescue_from ActiveRecord::RecordNotFound, with: (lambda do
+    render 'shared/not_found', status: 404
+  end)
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,46 @@
 module ApplicationHelper
-  def bootstrap_class_for(flash_type)
-    { success: 'alert-success',
-      error: 'alert-danger',
-      alert: 'alert-warning',
-      notice: 'alert-info' }[flash_type.to_sym] || flash_type.to_s
-  end
-
   def flash_messages(_opts = {})
-    flash.each do |msg_type, message|
+    return if flash.empty?
+    flash.map do |msg_type, message|
       concat(
         content_tag(:div,
                     message,
                     class: "alert #{bootstrap_class_for(msg_type)}") do
           content_tag(:div, class: 'container') { concat message }
         end)
-    end
-    nil
+    end.join.html_safe
+  end
+
+  def page_title(hash = {})
+    returnable = title(hash)
+    content_for(:title) { returnable }
+    returnable
+  end
+
+  def destroy_link_to(path, options)
+    link_to t('action.destroy.link', item: options[:item]),
+            path,
+            destroy_link_to_options(item: options[:item])
+  end
+
+  private
+
+  def destroy_link_to_options(item)
+    { method: :delete,
+      class: 'btn btn-danger btn-sm',
+      'data-confirm' => t('action.destroy.confirm.body', item),
+      'data-confirm-fade' => true,
+      'data-confirm-title' => t('action.destroy.confirm.title', item),
+      'data-confirm-cancel' => t('action.destroy.confirm.cancel', item),
+      'data-confirm-cancel-class' => 'btn-default',
+      'data-confirm-proceed' => t('action.destroy.confirm.proceed', item),
+      'data-confirm-proceed-class' => 'btn-danger' }
+  end
+
+  def bootstrap_class_for(flash_type)
+    { success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-info' }[flash_type.to_sym] || flash_type.to_s
   end
 end
