e8eb5cd2a53fc74ef8a462f1e22c5bd3504f4816
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-11-02 15:56:07 +1300

    pagination fix for decorators

--- app/decorators/pagination_decorator.rb
+++ app/decorators/pagination_decorator.rb
@@ -1,3 +1,3 @@
-class PaginatingDecorator < Draper::CollectionDecorator
+class PaginationDecorator < Draper::CollectionDecorator
   delegate :current_page, :total_pages, :limit_value, :entry_name, :total_count
 end
