d9c0c4a2f25e19f8e1c7ea9f5a9e91b74554d0b0
Najtmare <millan@sino.net>
2014-11-18 20:57:54 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
 User::Provider.create(name: 'facebook')
 User::Provider.create(name: 'twitter')
 User::Provider.create(name: 'google_oauth2')
+
+Category.create(name: 'Applied Art', slug: 'applied')
+Category.create(name: 'Digital Art', slug: 'digital')
+Category.create(name: 'Literature', slug: 'literature')
+Category.create(name: 'Performing Art', slug: 'performing')
+Category.create(name: 'Public Art', slug: 'public')
+Category.create(name: 'Music', slug: 'music')
+Category.create(name: 'Visual Art', slug: 'visual')
+Category.create(name: 'Other Art', slug: 'other')
+
+SubCategory.create(name: 'Architecture', slug: 'architecture', category_id: 1)
+SubCategory.create(name: 'Arts & Crafts', slug: 'crafts', category_id: 1)
+SubCategory.create(name: 'Decorative Arts', slug: 'decorative', category_id: 1)
+SubCategory.create(name: 'Data Art', slug: 'data', category_id: 2)
+SubCategory.create(name: 'Comic', slug: 'comic', category_id: 3)
+
+
