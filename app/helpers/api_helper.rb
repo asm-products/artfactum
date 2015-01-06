853664397a90220715937d0e9a2a5f087ae44d45
themetric <marklinstop@gmail.com>
2015-01-06 08:15:12 -0800

    add gallery API controllers

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -2,4 +2,7 @@ module ApiHelper
   def user_attributes
     [:id, :first_name, :last_name, :email, :avatar_url, :authentication_token, :created_at, :updated_at]
   end
+  def gallery_attributes
+    [:id, :user_id, :title]
+  end
 end
